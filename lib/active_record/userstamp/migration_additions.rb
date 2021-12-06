20120c03bd40b894c50e423e00fa291c072fb509
Chris Branson <138595+chrisbranson@users.noreply.github.com>
2021-12-06 16:55:02 +0000

    Use double splat for Ruby 3.x compatibility

--- lib/active_record/userstamp/migration_additions.rb
+++ lib/active_record/userstamp/migration_additions.rb
@@ -1,11 +1,11 @@
 module ActiveRecord::Userstamp::MigrationHelper
   extend ActiveSupport::Concern
 
-  def userstamps(*args)
+  def userstamps(**args)
     config = ActiveRecord::Userstamp.config
-    column(config.creator_attribute, :integer, *args)
-    column(config.updater_attribute, :integer, *args)
-    column(config.deleter_attribute, :integer, *args) if config.deleter_attribute.present?
+    column(config.creator_attribute, :integer, **args)
+    column(config.updater_attribute, :integer, **args)
+    column(config.deleter_attribute, :integer, **args) if config.deleter_attribute.present?
   end
 end
 
